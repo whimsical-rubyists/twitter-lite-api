64e6f16be63b67e26bcfdae161f0ccd9f3c15a99
thoth-ky <28805113+thoth-ky@users.noreply.github.com>
2019-10-20 23:26:27 +0300

    SET csrf tokens and rescue from errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,16 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format.json? }
+
+  before_action :set_csrf_cookie
   include SessionsHelpers
+
+  rescue_from ActionController::InvalidAuthenticityToken do |exception|
+    log_out
+    render { message : "Your session has been revoked" }
+  end
+
+  def set_csrf_cookie
+    cookies["CSRF-TOKEN"] = form_authenticity_token
+  end
 end
