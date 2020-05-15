25c8d9be038fca31405c84fa6c2a9fd4568818d9
Thoth Ky <28805113+thoth-ky@users.noreply.github.com>
2020-05-15 21:07:31 +0300

    update bundle and ruby version

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,17 +31,18 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  host = 'localhost:3000'
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {:host => "localhost:3000", protocol: "http"}
+  config.action_mailer.default_url_options = {host: 'localhost:3000', protocol: "http"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    # domain: 'gmail.com',
-    username: 'gloriaodipo@gmail.com',
-    password: 'Inviolata_26',
+    username: Rails.application.credentials.dig(:gmail, :username),
+    password: Rails.application.credentials.dig(:gmail, :password),
     authentication: 'plain',
     enable_starttls_auto: true
   }
